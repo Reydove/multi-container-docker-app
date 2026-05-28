c9d252d18ff5da10bd230bdefa700c2ef6c2802a
Mobolaji Habib <mobolajihabib555@gmail.com>
2026-05-28 07:24:20 +0100

    Updated File with addition of a database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Backend API Running');
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://database:27017/tasksdb')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const TaskSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find();
+
+  if (tasks.length === 0) {
+    const sampleTask = await Task.create({
+      title: 'Learn Docker Compose',
+      completed: false
+    });
+
+    return res.json([sampleTask]);
+  }
+
+  res.json(tasks);
 });
 
 app.listen(5000, () => {
